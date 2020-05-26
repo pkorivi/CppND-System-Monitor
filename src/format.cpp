00049d7682d12ba5715cda6f461002fa462c5bb4
pradeep korivi <pradeep.korivi@partner.bmw.de>
2020-05-26 18:14:22 +0200

    updated UpTime

--- src/format.cpp
+++ src/format.cpp
@@ -1,3 +1,5 @@
+#include <iomanip>
+#include <sstream>
 #include <string>
 
 #include "format.h"
@@ -8,4 +10,13 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds[[maybe_unused]]) {
+  std::stringstream result;
+  long hours = seconds / 3600;
+  long remaining_seconds = seconds % 3600;
+  long minutes = remaining_seconds / 60;
+  long last_seconds = remaining_seconds % 60;
+  result << std::setw(2) << std::setfill('0') << hours << ":" << minutes << ":"
+         << last_seconds;
+  return result.str();
+}
